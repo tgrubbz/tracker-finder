85eb9bb5e997ce7fdfa9b22b20647b65ea880429
tgrubbz <tgrubb.code@gmail.com>
2025-03-11 19:32:05 -0400

    add some tracker names including a test one I should be able to easily find.

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function run() {
-    alert('run!');
+    // map of tracker names and sources.
+    const trackers = {
+        "NaviStone": ["datasteam.io","murdoog.com/onetag"],
+        "Pebble Post": ["pbbl.co"],
+        "Remarketable/ALC": ["alcmpn.com"],
+        "LSDirect/Boomerang/4Cite": ["securedvisit.com"],
+        "El Toro": ["adnxs.com"],
+        "Postie": ["postaffiliatepro.com"],
+        "TEST": ["www.gstatic.com"]
+    };
+
+    for (let tracker in trackers) {
+        let terms = trackers[tracker];
+        for (let i = 0; i < terms.length; ++i) {
+            let elementsWithSrc = document.querySelectorAll(`[src*="${terms[i]}"]`);
+            if (elementsWithSrc.length > 0) {
+                console.log(`Found ${tracker} tag on ${window.location.hostname}`);
+                break;
+            }
+        }
+        console.log(`None found for ${tracker} tag on ${window.location.hostname}`);
+    }
 }
