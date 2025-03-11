7e5d1d8b550695426ba6566340bc4f7ef3f1bc9e
tgrubbz <tgrubb.code@gmail.com>
2025-03-11 19:43:38 -0400

    popout the results to another window. include the host for the results.

--- script.js
+++ script.js
@@ -10,15 +10,54 @@ function run() {
         "TEST": ["www.gstatic.com"]
     };
 
+    let results = {};
+    const host = window.location.hostname;
+
     for (let tracker in trackers) {
+        results[tracker] = { found: false };
         let terms = trackers[tracker];
         for (let i = 0; i < terms.length; ++i) {
             let elementsWithSrc = document.querySelectorAll(`[src*="${terms[i]}"]`);
             if (elementsWithSrc.length > 0) {
-                console.log(`Found ${tracker} tag on ${window.location.hostname}`);
+                results[tracker].found = true;
                 break;
             }
         }
-        console.log(`None found for ${tracker} tag on ${window.location.hostname}`);
     }
+    
+    // Create table
+    let table = document.createElement('table');
+    table.style.border = '1px solid black';
+    table.style.borderCollapse = 'collapse';
+    table.style.width = '80%';
+    table.style.margin = '20px auto';
+    table.innerHTML = `
+        <thead>
+            <tr>
+                <th style="border: 1px solid black; padding: 8px;">Tracker</th>
+                <th style="border: 1px solid black; padding: 8px;">Found?</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    `;
+
+    // Populate table
+    let tbody = table.querySelector('tbody');
+    for (let tracker in results) {
+        let row = document.createElement('tr');
+        row.innerHTML = `
+            <td style="border: 1px solid black; padding: 8px;">${tracker}</td>
+            <td style="border: 1px solid black; padding: 8px;">${(tracker.found ? 'Y' : 'N')}</td>
+        `;
+        tbody.appendChild(row);
+    }
+
+    let title = document.createElement('div');
+    title.innerHTML = `<h1>Trackers found on ${host}</h1>`;
+
+    // Create a new window
+    let w = window.open("");
+    w.document.body.appendChild(title);
+    w.document.body.appendChild(table);
 }
